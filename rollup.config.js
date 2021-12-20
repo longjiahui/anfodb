dcab769a454a7ffcf39a6948f69107b903206fb5
anfo <longjiahui@hotmail.com>
2021-12-20 22:31:22 +0800

    ff

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,18 @@
+import del from 'rollup-plugin-delete'
+import copy from 'rollup-plugin-copy'
+
 export default {
     input: 'src/db.js',
     output: {
         dir: 'dist',
         format: 'umd',
         name: 'AnfoDB',
-    }
+    },
+    plugins: [
+        del({targets: 'dist/*'}),
+        copy({targets: [{
+            src: 'src/debug.html',
+            dest: 'dist/'
+        }]})
+    ]
 }
